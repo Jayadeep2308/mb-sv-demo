aa4de5be99786f06c6438dee14d7e01a3903950e
Jayadeep <jayadeep.yarlagadda@ingka.com>
2020-04-18 15:12:38 +0200

    initial commit

---
+++ src/main/java/com/demo/mb/mbsvdemo/MbSvDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.mb.mbsvdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbSvDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbSvDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/mb/mbsvdemo/MbSvDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.mb.mbsvdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbSvDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
